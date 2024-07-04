f8c66b214811273b208e0b23045322fc34866a49
陳宏威 <ert60213@gmail.com>
2024-07-04 16:55:38 +0800

    add template, boostrap

--- products/views.py
+++ products/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Person
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    persons = Person.objects.all()
+    return render(
+        request,
+        "index.html",
+        {"persons": persons},
+    )
 
 
 def newproduct(request):
